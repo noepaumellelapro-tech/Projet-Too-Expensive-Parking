9af6001968e157ffe971c62dd169e1a9e4f19f52
Athiran NAGATHURAI <athiran.nagathurai@gmail.com>
2025-11-10 11:57:56 +0100

    ajout du ramdom

--- random.c
+++ random.c
@@ -1,21 +1,15 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "random.h"
 #include "vehicule.h"
-
 void random_car(Vehicule **liste) {
 
-    // Générer 4 véhicules aléatoires
-    for(int i =0; i<4; i++){
     int couleurs         = rand() % 5; 
     int vitesse          = rand() % 5; 
     char type            = (rand() % 2)  ? 'C' : 'V'; // C pour voiture, V pour camion
     char alignement      = (rand() % 2)  ? 'G' : 'D'; // G pour gauche, D pour droite 
+    
 
-     Vehicule* v = creerVehicule(0, 0, 'N', vitesse, couleurs, alignement, type, 'M', 0, (char[2][3]){{' ', 'A', ' '}, {' ', ' ', ' '}});
+     Vehicule *v = creerVehicule(15, 15, 'N', vitesse, couleurs, alignement, type, 'M', 0,(char[2][3]){{'R', 'G', 'G'}, {'R', 'G', 'G'}});
      ajoutervehicule(liste, v);
-
-     return;
-    }
 
 }
